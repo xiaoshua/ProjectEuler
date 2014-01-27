4d4148a7ba90d1078362bd0307175dcfb6f2bf3c
WuEnci <xiaoshuaxs@gmail.com>
2014-01-27 16:22:21 +0800

    Update 173 hollow.cpp

--- 101-200/173 hollow.cpp
+++ 101-200/173 hollow.cpp
@@ -9,9 +9,7 @@ int main() {
     long long left, right, mid, mi, i;
     long long ans=0;
     for(i=3; ; ++i) {
-        left=1;
-        right=i-1;
-        mi=-1;
+        left=1; right=i-1; mi=-1;
         while(left<=right) {
             mid=(left+right)>>1;
             if(i*i-mid*mid<=N) {
@@ -21,8 +19,12 @@ int main() {
                 left=mid+1;
             }
         }
-        if(mi==-1) break;
-        if((i^mi)&1) ++mi;
+        if(mi==-1) {
+            break;
+        }
+        if((i^mi)&1) {
+            ++mi;
+        }
         ans += (i-mi)/2;
     }
     cout<<ans<<endl;
